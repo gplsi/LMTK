4fe807fa85683cae49bf771ca7316c8ae3e84393
EEstevanell <ernestoestevanellv@gmail.com>
2025-02-18 09:54:53 +0000

    Refactor tokenization schema to include tokenizer properties and enhance dataset configuration; update tokenization execution workflow with detailed comments

--- src/tasks/tokenization/__init__.py
+++ src/tasks/tokenization/__init__.py
@@ -3,3 +3,22 @@
 def execute(config):
     print("Tokenizing with the following config:")
     print(config)
+    
+    # input dataset
+    # - huggingface
+    # - local 
+    #   - transformers dataset
+    #   - files
+    #       - csv
+    #       - txt
+    #       - json
+    
+    # workflow
+    # if dataset from files and not transformer dataset
+    # - load from files
+    # - create dataset
+    # - save to disk (if specified)
+    
+    # then for all datasets
+    # - tokenize
+    # - save to disk (if specified)
