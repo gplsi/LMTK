e2cdd9a03e7c4cec5e4ec81f55cbbb9deff44f27
sgs97ua <santiago.gsegura@gmail.com>
2025-10-17 13:10:07 +0200

    Update param to --tmp to not use inode memory in SLURM and avoid exceptions of not memory available

--- src/tasks/training/fabric/trainer/base.py
+++ src/tasks/training/fabric/trainer/base.py
@@ -244,6 +244,7 @@ class FabricTrainerBase(ABC):
                     progress_info = f" (step {global_iteration} in epoch {current_epoch})"
             
             self.cli_logger.info(f"Saving checkpoint to {checkpoint_name!r}{progress_info}")
+            self.cli_logger.debug(f"Checkpoint state: {self.state}")
             fabric.save(output_checkpoint_path, self.state)
             self.cli_logger.debug(f"Checkpoint saved successfully to {str(output_checkpoint_path)}")
             
