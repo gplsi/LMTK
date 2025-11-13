0bf7d68448fc4ed5bd700b4c50922e3ebf30fd92
sgs97ua <sgs97@gcloud.ua.es>
2025-11-13 09:13:02 +0100

    Modify conda_enviroment source

--- scripts/set_environment.sh
+++ scripts/set_environment.sh
@@ -5,8 +5,8 @@ fi
 
 echo "Setting up environment..."
 
-if [[ -f /leonardo/home/userexternal/sgaliano/miniconda3/etc/profile.d/conda.sh ]]; then
-  source /leonardo/home/userexternal/sgaliano/miniconda3/etc/profile.d/conda.sh
+if [[ -f /leonardo_work/EUHPC_D22_034/miniconda3/etc/profile.d/conda.sh ]]; then
+  source /leonardo_work/EUHPC_D22_034/miniconda3/etc/profile.d/conda.sh
 else
   echo "Error: conda initialization script not found." >&2
   return 1
